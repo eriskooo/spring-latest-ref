13fb48ce52b2887fbf08f7194edf29bae292db67
lorman erich <lorman.erich@scheidt-bachmann.sk>
2026-01-27 08:11:41 +0100

    Initial commit

---
+++ src/main/java/com/lorman/ref/spring/SpringLatestRefApplication.java
@@ -0,0 +1,13 @@
+package com.lorman.ref.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLatestRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLatestRefApplication.class, args);
+	}
+
+}
